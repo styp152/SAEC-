2f6ff3b021c8772f9c70bff5462020d944dfa0f0
Typson <styp152@gmail.com>
2011-05-22 00:58:30 -0430

    Incluyendo cambios en facturacion, inventario y administracion

--- administracion.php
+++ administracion.php
@@ -3,6 +3,70 @@ include("head.html");
 include("menu.html");
 ?>
 <h2>Administracion</h2>
+<!-- TODO Buscar los articulos agotados -->
+<!-- TODO Buscar las ventas en la ultima semana -->
+<h3>Productos Agotados</h3>
+<table>
+  <tr class="first">
+    <td>Nombre </td>
+    <td>Descripcion </td> 
+    <td>Precio Unitario Bsf</td>
+  </tr>
+  <tr>
+    <td>Tazas</td>
+    <td>Tazas Termicas que se revelan con el calor</td>
+    <td>50 </td>
+  </tr>
+  <tr>
+    <td>Chapas</td>
+    <td>Tamaño Mediano </td>
+    <td>5 </td>
+  </tr>
+  <tr>
+    <td>Camisas S</td>
+    <td>Camisas de Poliester Talla S </td>
+    <td>60 </td>
+  </tr>
+</table>
+<br />
+<h3>Produccion de la Ultima Semana</h3>
+<table>
+  <tr class="first">
+    <td>Fecha </td>
+    <td>Cantidad de Ventas </td>
+    <td>Monto de la Venta </td>
+  </tr>
+  <tr>
+    <td>Lunes 16/05</td>
+    <td>5</td>
+    <td>550 </td>
+  </tr>
+  <tr>
+    <td>Martes 17/05</td>
+    <td>6</td>
+    <td>550 </td>
+  </tr>
+  <tr>
+    <td>Miercoles 18/05</td>
+    <td>8</td>
+    <td>550 </td>
+  </tr>
+  <tr>
+    <td>Jueves 19/05</td>
+    <td>10</td>
+    <td>550 </td>
+  </tr>
+  <tr>
+    <td>Viernes 20/05</td>
+    <td>7</td>
+    <td>550 </td>
+  </tr>
+  <tr>
+    <td>Sabado 21/05</td>
+    <td>4</td>
+    <td>550 </td>
+  </tr>
+</table>
 </div>
 <?php
 include("menuAdministracion.html");
--- facturacion.php
+++ facturacion.php
@@ -3,7 +3,33 @@ include("head.html");
 include("menu.html");
 ?>
 <h2>Facturacion</h2>
-<!-- TODO total de factura y el monto del dia-->
+<!-- TODO consulta a la base de datos para buscar las facturas del dia -->
+<!-- TODO calcular las ventas echa por cada trabajador y el total del dia de ventas -->
+<br />
+<table>
+  <tr class="first">
+    <td>Vendedor </td>
+    <td>Cantidad de Ventas </td>
+    <td>Total Vendido en Bsf</td> 
+  </tr>
+  <tr>
+    <td>Maria Perez</td>
+    <td>5</td>
+    <td>750</td>
+  </tr>
+  <tr>
+    <td>Nelson Garcia</td>
+    <td>2</td>
+    <td>300</td>
+  </tr>
+  <tr>
+    <td>Gabriel Albornoz</td>
+    <td>1 </td>
+    <td>240 </td>
+  </tr>
+</table>
+<br />
+<h3>Total Vendido del Dia: 1290 Bsf</h3>
 </div>
 <?php
 include("menuFacturacion.html");
