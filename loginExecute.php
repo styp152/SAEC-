5fa4f8a72e26351da1da7f33e9758e674fe2383c
Typson <styp152@gmail.com>
2011-05-24 02:47:24 -0430

    Trabajando sobre el Login de la Aplicacion

--- loginExecute.php
+++ loginExecute.php
@@ -1,21 +1,22 @@
 <?php
-// Realizar login segun sea el Usuario y Clave  XXX|TODO
-// Realizar el guardado de datos en la Sesion  XXX|TODO
 session_start();
 include('libreria.php');
+include('db/searchs.php');
 include('clases/Vendedor.php');
 conectarDB();
 $vendedor= new Vendedor();
 $vendedor->updateDatos($_REQUEST);
-if($vendedor->getCedula()=='17129464' and $vendedor->getClave()=='9464'){
+$vendedorDB=buscarVendedorPorCedulaClaveSinCifrar($vendedor->getCedula());
+echo $vendedorDB->getClave(). "++++++" .$vendedor->getClave();
+if($vendedor->getCedula()==$vendedorDB->getCedula() and $vendedor->getClave()==$vendedorDB->getClave()){
   $_SESSION["log"]="1";
-  $_SESSION["vendedor"]= $vendedor;
+  $_SESSION["vendedor"]= $vendedorDB;
   //$_SESSION["cedula"] = $person->cedula;
   //$_SESSION["Clave"] = $person->clave;
-  $_SESSION["Nivel"] = 2;// TODO completar la busqueda y la asignacion correcta
-  header("location:index.php");
+  $_SESSION["Nivel"] = $vendedorDB->getNivel();
+  //header("location:index.php");
 }
 else{
-  header('location:index.php');
+  //header('location:index.php');
 }
 ?>
